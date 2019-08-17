619605336b1ded1002a93a1b0352c6663d263fc9
MindPower99 <gabriel-jorge1999@gmail.com>
2019-08-17 18:21:52 -0300

    folha de exercicios numero 1

---
+++ curso_js/js/ex1.js
@@ -0,0 +1,30 @@
+// Folha de Exercícios nº1
+
+// 1)
+console.log("O arquivo ex1.js está funcionando");
+
+// 2)
+var ano = 1999;
+var idade = 2019 - ano;
+var nome = "Gabriel";
+console.log('Olá, eu me chamo ' + nome + ' e tenho ' + idade +' anos e estou estudando Javascript.');
+
+// 3)
+var nomeAluno = 'Gabriel';
+var nMatricula = '18456';
+var n1 = 10;
+var n2 = 10;
+
+console.log('O aluno ' + nomeAluno + ', Matricula ' + nMatricula + ', obteve a média final de ' + (n1 + n2) / 2);
+
+// 4)
+var telefone = '998701209';
+
+if (telefone[8] != null) {
+	console.log("Resultado do teste: true");
+}
+else
+	console.log("Resultado do teste: false");
+
+// 5)
+console.log(Math.pow(32,6));
