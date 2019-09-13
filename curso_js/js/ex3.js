d68f9cbf40b6f0a08385133b31c58517669aaf1d
MindPower99 <gabriel-jorge1999@hotmail.com>
2019-09-12 22:34:08 -0300

    Folha de exercícios nº 3 - Exercício 3

--- curso_js/js/ex3.js
+++ curso_js/js/ex3.js
@@ -19,3 +19,36 @@ while (ano_copa < 2019) {
 	document.getElementById("anos_copa").innerHTML += '<li>' + ano_copa + '</li>';
 	ano_copa += 4;
 }	
+
+// 3)
+
+
+
+function calcular_nota() {
+	
+	var nota1 = parseFloat(document.getElementById("nota1").value);
+	console.log(nota1);
+	var nota2 = parseFloat(document.getElementById("nota2").value);
+	var faltas = document.getElementById("n_faltas").value;
+	//console.log(faltas);
+	var percentual = parseFloat((20 - faltas) / 20) * 100;
+	console.log(percentual);
+	var media = (nota1 + nota2) / 2;
+	console.log(media);
+	if (media >= 6.5 && percentual >= 70) {
+		document.getElementById("result").innerHTML = '<strong> aprovado </strong>';
+	}
+	else if (media < 6.5 && percentual < 70) {
+		document.getElementById("result").innerHTML = '<strong> reprovado por média insuficiente e por falta </strong>';
+	}
+	else if (media < 6.5) {
+		document.getElementById("result").innerHTML = '<strong> reprovado por média insuficiente </strong>';
+	}
+	else if (percentual < 70) {
+		document.getElementById("result").innerHTML = '<strong> reprovado por falta </strong>';
+	}
+
+	
+	
+	
+}
