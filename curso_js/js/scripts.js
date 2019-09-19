acd47deb63907b274d731281c059b18f20320309
MindPower99 <gabriel-jorge1999@hotmail.com>
2019-09-19 20:22:58 -0300

    Aula 33 - Evento onchange

--- curso_js/js/scripts.js
+++ curso_js/js/scripts.js
@@ -822,6 +822,8 @@ window.setTimeout(function () {
 	*/
 	
 	// Date
+	
+	/*
 	document.getElementById("mostrar_data").onclick = function () {
 		var data_select = document.getElementById("data_evento").value;
 		
@@ -829,6 +831,39 @@ window.setTimeout(function () {
 		
 		document.getElementById("data_selecionada").innerHTML = data_completa;
 	}
+	
+	
+	
+	// Aula 33 - evento onchange
+	
+	document.getElementById("escolaridade").onchange = function () {
+		
+		var campo_select = document.getElementById("escolaridade");
+		var indice_selecionado = campo_select.options.selectedIndex;
+		var valor_selecionado = campo_select.options[indice_selecionado].innerHTML;
+		document.getElementById("escolaridade_selecionada").innerHTML = valor_selecionado;
+		
+	}
+	*/
+	
+	var check = document.getElementsByName("lanche");
+
+    for (var a = 0;  a < check.length; a++) {
+
+        check[a].onchange = function () {
+
+            document.getElementById("check_selecionado").innerHTML = "";
+            
+            for (var b = 0;  b < check.length; b++) {
+                
+                if (check[b].checked) {
+                    document.getElementById("check_selecionado").innerHTML += '<li>' + check[b].value + '</li>';
+                }
+            }
+        }  
+    }
+	
+	
 
 
 
