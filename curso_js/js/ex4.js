a484a7d54f6a6949e61ddeb3de66a02cd77eafb4
MindPower99 <gabriel-jorge1999@hotmail.com>
2019-09-20 20:17:03 -0300

    Folha de Exercícios nº4 - Exercício 1 / completo

--- curso_js/js/ex4.js
+++ curso_js/js/ex4.js
@@ -1,28 +1,17 @@
 // Folha de Exercicios nº4
 
 // 1)
+
+window.onload = function() {
+	if (localStorage.estado) {
+		var local_estado = localStorage.estado;
+		console.log(local_estado);
+		document.getElementById("estados").value = local_estado;
+		
+	}
+}
+
 	document.getElementById("estados").onchange = function () {
-		
-		if (localStorage.estado) {
-			var pre_estado = document.getElementById("estados").value;
-			
-			var campo_select = localStorage.estado;
-			var indice_selecionado = campo_select.options.selectedIndex;
-			var estado_selecionado = campo_select.options[indice_selecionado].innerHTML;
-			
-			window.localStorage.setItem("estado", estado_selecionado);
-			
-			localStorage.estado = pre_estado;
-			
-			console.log(pre_estado);
-			
-			// localStorage.estado
-			
-			
-			
-		}
-		
-		else {
 			
 			var campo_select = document.getElementById("estados");
 			var indice_selecionado = campo_select.options.selectedIndex;
@@ -30,83 +19,6 @@
 			
 			window.localStorage.setItem("estado", estado_selecionado);
 			
-			
-			
 			console.log(estado_selecionado);
-			
-		}
 	}
 
-
-	/*
-	document.getElementById("mostrar_opcao").onclick = function () {
-		
-		var campo_select = document.getElementById("options");
-		var indice_selecionado = campo_select.options.selectedIndex;
-		var valor_selecionado = campo_select.options[indice_selecionado].innerHTML;
-		
-		document.getElementById("opcao_selecionada").innerHTML = valor_selecionado;
-	}
-	
-	*/
-	
-	/*
-
-window.localStorage.setItem("nome","João");
-
-console.log(localStorage['nome']);
-
-*/
-
-/*
-console.log(localStorage['nome']);
-
-localStorage.removeItem("nome");
-
-console.log(localStorage['nome']);
-
-*/
-
-/*
-document.getElementById("enviar-nome").onclick = function () {
-
-  //guardar o nome digitado em local storage
-  var nome = document.getElementById("nome-usuario").value;
-  localStorage.setItem("nome", nome);
-
-  //esconder o formulário
-
-  document.getElementById("name-field").style.display = "none";
-
-  //atualizar e mostrar mensagem
-  document.getElementById("welcome-text").innerHTML = "Olá " + localStorage.nome + " ,tudo bem ?";
-  document.getElementById("not-me").innerHTML = "Não é " + localStorage.nome + " ?"
-  document.getElementById("welcome-area").style.display = "initial";
-
-
-};
-
-if(localStorage.nome) {
-
-  //esconder o formulário
-  document.getElementById("name-field").style.display = "none";
-
-  //atualizar e mostrar mensagem
-  document.getElementById("welcome-text").innerHTML = "Olá " + localStorage.nome + " ,tudo bem ?";
-  document.getElementById("not-me").innerHTML = "Não é " + localStorage.nome + " ?"
-  document.getElementById("welcome-area").style.display = "initial";
-}
-
-document.getElementById("not-me").onclick = function() {
-
-  // remover chave nome do local storage
-  localStorage.removeItem("nome");
-
-  //esconder mensagem de boas vindas
-  document.getElementById("welcome-area").style.display = "none";
-
-  // mostrar formulário
-  document.getElementById("name-field").style.display = "initial";
-
-}
- */
