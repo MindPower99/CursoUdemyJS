ac044232ee9c67768da595c5233869c14119d1a3
MindPower99 <gabriel-jorge1999@hotmail.com>
2019-09-20 22:00:04 -0300

    Folha de Exercícios nº4 - Exercício 2 / incompleto

--- curso_js/js/ex4.js
+++ curso_js/js/ex4.js
@@ -22,3 +22,59 @@ window.onload = function() {
 			console.log(estado_selecionado);
 	}
 
+// 2)
+
+document.getElementById("confirmar_pedido").onclick = function () {
+	
+	var data = new Date();
+	var tipo_envio = document.getElementById("envios");
+	var envio_selecionado = tipo_envio.options.selectedIndex;
+	var envio = tipo_envio.options[envio_selecionado].value;
+	
+	window.localStorage.setItem("tipo_envio", envio);
+	//toString(estado_selecionado);
+	
+	if(envio == "normal") {
+		console.log(data);
+		var dias_normal = new Date(19)
+		var dia_mes = new Date().getDate();
+		console.log(dia_mes);
+		var prazo_entrega = dia_mes + 12;
+		console.log(prazo_entrega);
+		
+		
+
+	}
+	
+	document.getElementById("data_pedido").innerHTML = getDate() + '/' + getMouth() + '/' + getFullYear();
+	
+	
+}
+
+/*
+var data_hoje = new Date();
+console.log(data_hoje.getDate());
+
+var data_nascimento = "1980-03-01";
+
+var ano_nascimento = new Date("1980-03-01").getFullYear();
+var ano_atual = new Date().getFullYear();
+var idade = ano_atual - ano_nascimento;
+console.log(idade);
+
+
+var data = new Date();
+
+console.log(data.getTime() / 31536000000);
+
+
+var envio = new Date("2018-03-20");
+envio = envio.getTime();
+
+var entrega = new Date("2018-04-06");
+entrega = entrega.getTime();
+
+var dias = (entrega - envio) / 86400000;
+
+document.getElementById("dias_entrega").innerHTML = dias;
+*/
